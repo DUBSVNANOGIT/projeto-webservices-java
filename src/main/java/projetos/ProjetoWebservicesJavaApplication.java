eec5bda0a1a35dbc7539a892dabe4947efe0b05d
DUBSVNANOGIT <arthur.bic_@hotmail.com>
2024-08-01 19:36:00 -0300

    Setting up

---
+++ src/main/java/projetos/ProjetoWebservicesJavaApplication.java
@@ -0,0 +1,13 @@
+package projetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebservicesJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoWebservicesJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/projetos/ProjetoWebservicesJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package projetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebservicesJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
