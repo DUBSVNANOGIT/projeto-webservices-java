f82f3c039000695c8b6370348cf67df9f1686bf7
DUBSVNANOGIT <arthur.bic_@hotmail.com>
2024-08-03 13:56:32 -0300

    H2 database, test profile, JPA

--- src/main/java/projetos/entities/User.java
+++ src/main/java/projetos/entities/User.java
@@ -1,21 +1,23 @@
 package projetos.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
 
-
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
-
-    public User(Long id){
-        this.id = id;
-    }
 
     public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
@@ -23,6 +25,9 @@ public class User implements Serializable {
         this.email = email;
         this.phone = phone;
         this.password = password;
+    }
+
+    public User() {
     }
 
     public Long getId() {
