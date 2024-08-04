b26c07aa0ff7977636ad7abd9ef019c8ed4f6c15
DUBSVNANOGIT <arthur.bic_@hotmail.com>
2024-08-04 00:03:27 -0300

    Product-OrderItem one-to-many association Annotations

--- src/main/java/projetos/entities/OrderItem.java
+++ src/main/java/projetos/entities/OrderItem.java
@@ -1,13 +1,19 @@
 package projetos.entities;
 
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import projetos.entities.pk.OrderItemPK;
 
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_order_item")
 public class OrderItem implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @EmbeddedId
     private OrderItemPK id;
     private Integer quantity;
     private Double price;
